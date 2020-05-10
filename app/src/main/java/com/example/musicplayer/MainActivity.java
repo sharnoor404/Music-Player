600969619444efed7bc603436511a12e558c28c3
sharnoor404 <sharnoor404@gmail.com>
2020-05-10 12:14:56 +0530

    circular image library added

--- app/src/main/java/com/example/musicplayer/MainActivity.java
+++ app/src/main/java/com/example/musicplayer/MainActivity.java
@@ -11,4 +11,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+   //step 1:change action bar color by changin the primary colors in color.xml
+   //step 2:to add circular image add dependencies in app gradle
+
+
 }
