3e9c62951737ed1a92d7e2bfe523afb6efadc2ec
sharnoor404 <sharnoor404@gmail.com>
2020-05-11 15:32:43 +0530

    media player

--- app/src/main/java/com/example/musicplayer/MainActivity.java
+++ app/src/main/java/com/example/musicplayer/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.musicplayer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,6 +11,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
 
     Button play,pause,stop;
+    MediaPlayer mediaPlayer;
+    //to operate the media(music) here.
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,11 +23,24 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         pause=(Button)findViewById(R.id.btn_pause);
         stop=(Button)findViewById(R.id.btn_stop);
 
+        play.setOnClickListener(this);
+        pause.setOnClickListener(this);
+        stop.setOnClickListener(this);
+
+
     }
 
     @Override
     public void onClick(View v) {
-        
+        switch(v.getId()){
+            case R.id.btn_play:
+                break;
+            case R.id.btn_pause:
+                break;
+            case R.id.btn_stop:
+                break;
+
+        }
     }
     //step 1:change action bar color by changin the primary colors in color.xml
     //step 2:to add circular image add dependencies in app gradle
